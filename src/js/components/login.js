4d02841fe24d4ef892cce6edf759961cad35d9d6
cstallar <38629223+cstallar@users.noreply.github.com>
2018-05-29 18:50:10 -0400

    Updated Login CSS
    It looks better now

--- src/js/components/login.js
+++ src/js/components/login.js
@@ -6,11 +6,8 @@ export default class Login extends Component{
             <body>
             <form class="loginForm" action="login-submit.html" method="post">
                 <br></br><br></br>
-                <h3>Username</h3>
-                <br></br>
+                <p>Login</p>
                 <input class="usernameField" type="text" name="username" placeholder="Username" required></input><br></br>
-                <br></br>
-                <h3>Password</h3>
                 <br></br>
                 <input class="passwordField" type="password" name="password" placeholder="Password" required></input><br></br>
                 <br></br>
