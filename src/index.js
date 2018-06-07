7d71d278cebd22361037295981927ae8aeaeb7b7
Jason Liu <shadowbox826@gmail.com>
2018-06-07 19:42:47 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,8 @@ import  { BrowserRouter, Router, Route } from 'react-router-dom'
 import Navbar from './js/components/navbar';
 import About from './js/components/about';
 import Home from './js/components/home';
-<<<<<<< HEAD
 import Footer from './js/components/footer';
-=======
 import Login from './js/components/login';
->>>>>>> 3517af9493aa54c03eb911dfd1a72801218941b1
-
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
@@ -20,11 +16,8 @@ ReactDOM.render(
         <Navbar/>
         <Route exact path="/" component={ Home }/>
         <Route path="/about" component={ About }/>
-<<<<<<< HEAD
         <Footer/>
-=======
           <Route path="/login" component={ Login }/>
->>>>>>> 3517af9493aa54c03eb911dfd1a72801218941b1
       </div>
     </BrowserRouter>
   ), document.getElementById('root'));
